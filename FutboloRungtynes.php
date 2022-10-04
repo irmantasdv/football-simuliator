64d09b8b2a0db581e2bd47a74f85c20df4080ce0
irmantasdv <53112192+irmantasdv@users.noreply.github.com>
2022-10-04 13:53:41 +0200

    initial commit

---
+++ FutboloRungtynes.php
@@ -0,0 +1,17 @@
+<?php
+class FootballGame {
+    public $patirtaTrauma;
+    public $kuriKomandaLaimejo;
+    public $rungtyniuRezultatas;
+
+    public function simuliuotiTraumas(){
+
+    }
+
+    public function simuliotiRungtynes(){
+        
+    }
+}
+
+
+?>
